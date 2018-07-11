350951186a53d04bbf8c14274c43e27568d3782c
Camilo Sanchez <camilin87@gmail.com>
2018-07-10 22:00:22 -0400

    make the status page accessible

--- src/main/java/com/tddapps/controllers/Status.java
+++ src/main/java/com/tddapps/controllers/Status.java
@@ -1,28 +1,18 @@
 package com.tddapps.controllers;
 
-import java.util.Collections;
-import java.util.Map;
-
-import com.tddapps.controllers.response.ApiGatewayResponse;
-import com.tddapps.controllers.response.Response;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.tddapps.controllers.response.ApiGatewayResponse;
+
+import java.util.Map;
 
 public class Status implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = LogManager.getLogger(Status.class);
-
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: {}", input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
-				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.setObjectBody("OK")
 				.build();
 	}
 }
