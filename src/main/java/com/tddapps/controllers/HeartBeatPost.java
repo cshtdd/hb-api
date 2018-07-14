d8d191072ca1a9a100a6f8e201ca661902316d7a
Camilo Sanchez <camilin87@gmail.com>
2018-07-14 08:16:17 -0400

    start defining a new function to save the heartbeats

---
+++ src/main/java/com/tddapps/controllers/HeartBeatPost.java
@@ -0,0 +1,17 @@
+package com.tddapps.controllers;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.tddapps.controllers.response.ApiGatewayResponse;
+
+import java.util.Map;
+
+public class HeartBeatPost implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody("OK")
+                .build();
+    }
+}
