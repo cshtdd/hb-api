94dd3cdb66e8f31bf4c6058c31cc90ea338336ec
Camilo Sanchez <camilin87@gmail.com>
2018-07-15 17:17:13 -0400

    create an HttpJsonActionStub to help with the HttpJsonController testing

---
+++ src/test/java/com/tddapps/controllers/HttpJsonActionStub.java
@@ -0,0 +1,62 @@
+package com.tddapps.controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import static com.tddapps.utils.StringExtensions.*;
+
+public class HttpJsonActionStub implements HttpJsonAction<String, String>{
+    private BodyParseException seededParseException = null;
+    private String seededParsedBody = null;
+    private int seededStatusCode = -1;
+    private String seededResultBody = null;
+
+    @Override
+    public String parse(JsonNode body) throws BodyParseException {
+        if (seededParseException != null){
+            throw seededParseException;
+        }
+
+        return seededParsedBody;
+    }
+
+    @Override
+    public HttpJsonResponse<String> process(String body) {
+        if (!EmptyWhenNull(seededParsedBody).equals(EmptyWhenNull(body))){
+            throw new RuntimeException("Received unexpected process call");
+        }
+
+        return new HttpJsonResponse<>(seededStatusCode, seededResultBody);
+    }
+
+    public BodyParseException getSeededParseException() {
+        return seededParseException;
+    }
+
+    public void setSeededParseException(BodyParseException seededParseException) {
+        this.seededParseException = seededParseException;
+    }
+
+    public String getSeededParsedBody() {
+        return seededParsedBody;
+    }
+
+    public void setSeededParsedBody(String seededParsedBody) {
+        this.seededParsedBody = seededParsedBody;
+    }
+
+    public String getSeededResultBody() {
+        return seededResultBody;
+    }
+
+    public void setSeededResultBody(String seededResultBody) {
+        this.seededResultBody = seededResultBody;
+    }
+
+    public int getSeededStatusCode() {
+        return seededStatusCode;
+    }
+
+    public void setSeededStatusCode(int seededStatusCode) {
+        this.seededStatusCode = seededStatusCode;
+    }
+}
