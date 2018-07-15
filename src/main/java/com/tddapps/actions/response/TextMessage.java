f4c789304ea85b0a6b87cb79336ee354f206b428
Camilo Sanchez <camilin87@gmail.com>
2018-07-15 09:40:08 -0400

    create a TextMessage class for those actions that will return a string

---
+++ src/main/java/com/tddapps/actions/response/TextMessage.java
@@ -0,0 +1,13 @@
+package com.tddapps.actions.response;
+
+public class TextMessage {
+    private final String message;
+
+    public TextMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
