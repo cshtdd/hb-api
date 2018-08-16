0768674a01ff8337d3ffc2722464bcda8fc06856
Camilo Sanchez <camilin87@gmail.com>
2018-08-16 14:54:44 -0400

    start building infrastructure in the StatusGetAction to read the cache

--- src/main/java/com/tddapps/actions/StatusGetAction.java
+++ src/main/java/com/tddapps/actions/StatusGetAction.java
@@ -7,10 +7,14 @@ import com.tddapps.controllers.HttpSupplierAction;
 import com.tddapps.dal.DalException;
 import com.tddapps.dal.HeartBeat;
 import com.tddapps.dal.HeartBeatRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static com.tddapps.utils.DateExtensions.UtcNowPlusMs;
 
 public class StatusGetAction implements HttpSupplierAction<TextMessage> {
+    private static final Logger LOG = LogManager.getLogger(StatusGetAction.class);
+
     private final HeartBeatRepository heartBeatRepository;
 
     public StatusGetAction(HeartBeatRepository heartBeatRepository) {
@@ -19,6 +23,12 @@ public class StatusGetAction implements HttpSupplierAction<TextMessage> {
 
     @Override
     public HttpJsonResponse<TextMessage> process() throws ActionProcessException {
+        if (ShouldReturnCachedResponse()){
+            return HttpJsonResponse.Success(TextMessage.OK);
+        }
+
+        LOG.info("Cache miss");
+
         HeartBeat hb = new HeartBeat(getClass().getSimpleName(), UtcNowPlusMs(4*60*60*1000), true);
 
         try {
@@ -28,5 +38,9 @@ public class StatusGetAction implements HttpSupplierAction<TextMessage> {
         }
 
         return HttpJsonResponse.Success(TextMessage.OK);
+    }
+
+    private boolean ShouldReturnCachedResponse() {
+        return false;
     }
 }
