c0f821d0fa0705fe938a95772af5fee8018690f0
Camilo Sanchez <camilin87@gmail.com>
2018-08-16 07:58:43 -0400

    the Status Api checks DB connectivity

--- src/main/java/com/tddapps/actions/StatusGetAction.java
+++ src/main/java/com/tddapps/actions/StatusGetAction.java
@@ -4,12 +4,13 @@ import com.tddapps.actions.response.TextMessage;
 import com.tddapps.controllers.ActionProcessException;
 import com.tddapps.controllers.HttpJsonResponse;
 import com.tddapps.controllers.HttpSupplierAction;
+import com.tddapps.dal.DalException;
+import com.tddapps.dal.HeartBeat;
 import com.tddapps.dal.HeartBeatRepository;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import static com.tddapps.utils.DateExtensions.UtcNowPlusMs;
 
 public class StatusGetAction implements HttpSupplierAction<TextMessage> {
-    private static final Logger LOG = LogManager.getLogger(StatusGetAction.class);
     private final HeartBeatRepository heartBeatRepository;
 
     public StatusGetAction(HeartBeatRepository heartBeatRepository) {
@@ -18,6 +19,14 @@ public class StatusGetAction implements HttpSupplierAction<TextMessage> {
 
     @Override
     public HttpJsonResponse<TextMessage> process() throws ActionProcessException {
+        HeartBeat hb = new HeartBeat(getClass().getSimpleName(), UtcNowPlusMs(4*60*60*1000), true);
+
+        try {
+            heartBeatRepository.Save(hb);
+        } catch (DalException e) {
+            throw new ActionProcessException(e.getMessage());
+        }
+
         return HttpJsonResponse.Success(TextMessage.OK);
     }
 }
--- src/test/java/com/tddapps/actions/StatusGetActionTest.java
+++ src/test/java/com/tddapps/actions/StatusGetActionTest.java
@@ -3,27 +3,57 @@ package com.tddapps.actions;
 import com.tddapps.actions.response.TextMessage;
 import com.tddapps.controllers.ActionProcessException;
 import com.tddapps.controllers.HttpJsonResponse;
+import com.tddapps.dal.DalException;
+import com.tddapps.dal.HeartBeat;
 import com.tddapps.dal.HeartBeatRepository;
 import org.junit.jupiter.api.Test;
 
+import static com.tddapps.utils.DateExtensions.UtcNowPlusMs;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class StatusGetActionTest {
     private final HeartBeatRepository heartBeatRepository = mock(HeartBeatRepository.class);
     private final StatusGetAction action = new StatusGetAction(heartBeatRepository);
 
     @Test
-    public void ReturnsOk(){
-        assertEquals(HttpJsonResponse.Success(TextMessage.OK), process());
-    }
+    public void VerifiesHeartBeatsCanBeSaved(){
+        HeartBeat expectedHeartBeat = new HeartBeat(
+                "StatusGetAction",
+                UtcNowPlusMs(4*60*60*1000),
+                true
+        );
 
-    private HttpJsonResponse<TextMessage> process(){
         try {
-            return action.process();
+            HttpJsonResponse<TextMessage> result = action.process();
+
+            assertEquals(HttpJsonResponse.Success(TextMessage.OK), result);
+            verify(heartBeatRepository).Save(argThat(t -> t.almostEquals(expectedHeartBeat)));
         } catch (ActionProcessException e) {
             fail("Process should not have thrown", e);
-            return null;
+        } catch (DalException e) {
+            fail("Save should not have thrown", e);
         }
     }
+
+    @Test
+    public void ProcessThrowsAnActionProcessExceptionWhenTheHeartBeatCouldNotBeSaved(){
+        try {
+            doThrow(new DalException("Save failed"))
+                    .when(heartBeatRepository)
+                    .Save(any(HeartBeat.class));
+
+            action.process();
+            fail("Process Should have thrown an error");
+        } catch (ActionProcessException e) {
+            assertEquals("Save failed", e.getMessage());
+        } catch (DalException e) {
+            fail("Save should not have thrown", e);
+        }
+    }
+
 }
