748a55377c60df19355648a84622b8ff6d85c818
Camilo Sanchez <camilin87@gmail.com>
2018-08-16 07:47:31 -0400

    the StatusGetAction needs a HeartBeatRepository

--- src/main/java/com/tddapps/actions/StatusGetAction.java
+++ src/main/java/com/tddapps/actions/StatusGetAction.java
@@ -4,11 +4,17 @@ import com.tddapps.actions.response.TextMessage;
 import com.tddapps.controllers.ActionProcessException;
 import com.tddapps.controllers.HttpJsonResponse;
 import com.tddapps.controllers.HttpSupplierAction;
+import com.tddapps.dal.HeartBeatRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class StatusGetAction implements HttpSupplierAction<TextMessage> {
     private static final Logger LOG = LogManager.getLogger(StatusGetAction.class);
+    private final HeartBeatRepository heartBeatRepository;
+
+    public StatusGetAction(HeartBeatRepository heartBeatRepository) {
+        this.heartBeatRepository = heartBeatRepository;
+    }
 
     @Override
     public HttpJsonResponse<TextMessage> process() throws ActionProcessException {
--- src/test/java/com/tddapps/actions/StatusGetActionTest.java
+++ src/test/java/com/tddapps/actions/StatusGetActionTest.java
@@ -3,12 +3,15 @@ package com.tddapps.actions;
 import com.tddapps.actions.response.TextMessage;
 import com.tddapps.controllers.ActionProcessException;
 import com.tddapps.controllers.HttpJsonResponse;
+import com.tddapps.dal.HeartBeatRepository;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 public class StatusGetActionTest {
-    private final StatusGetAction action = new StatusGetAction();
+    private final HeartBeatRepository heartBeatRepository = mock(HeartBeatRepository.class);
+    private final StatusGetAction action = new StatusGetAction(heartBeatRepository);
 
     @Test
     public void ReturnsOk(){
