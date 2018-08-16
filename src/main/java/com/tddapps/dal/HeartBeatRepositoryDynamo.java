ab695df759ccd07e3e545962dcc65600fceb1891
Camilo Sanchez <camilin87@gmail.com>
2018-08-16 07:18:57 -0400

    the HeartBeatRepositoryDynamo properly handles AmazonClientException

--- src/main/java/com/tddapps/dal/HeartBeatRepositoryDynamo.java
+++ src/main/java/com/tddapps/dal/HeartBeatRepositoryDynamo.java
@@ -1,6 +1,12 @@
 package com.tddapps.dal;
 
+import com.amazonaws.AmazonClientException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class HeartBeatRepositoryDynamo implements HeartBeatRepository {
+    private static final Logger LOG = LogManager.getLogger(HeartBeatRepositoryDynamo.class);
+
     private final DynamoDBMapperFactory mapperFactory;
 
     public HeartBeatRepositoryDynamo(DynamoDBMapperFactory mapperFactory){
@@ -8,7 +14,12 @@ public class HeartBeatRepositoryDynamo implements HeartBeatRepository {
     }
 
     @Override
-    public void Save(HeartBeat heartBeat) {
-        mapperFactory.getMapper().save(heartBeat);
+    public void Save(HeartBeat heartBeat) throws DalException{
+        try {
+            mapperFactory.getMapper().save(heartBeat);
+        } catch (AmazonClientException e){
+            LOG.error("HeartBeat Save Error", e);
+            throw new DalException(e.getMessage());
+        }
     }
 }
