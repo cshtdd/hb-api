6e8edc347f7b257b6b5b7b73a521cbe23b2e3600
Camilo Sanchez <camilin87@gmail.com>
2018-08-18 09:31:34 -0400

    create a Settings class for all the naming constants

---
+++ src/main/java/com/tddapps/dal/Settings.java
@@ -0,0 +1,6 @@
+package com.tddapps.dal;
+
+public abstract class Settings {
+    public final static String TOPIC_NAME = "TOPIC_NAME";
+    public final static String TABLE_PREFIX = "TABLE_PREFIX";
+}
