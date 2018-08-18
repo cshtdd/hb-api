8ea358c8b3af453a7c1f47bbc613c38b75267094
Camilo Sanchez <camilin87@gmail.com>
2018-08-18 10:27:56 -0400

    send notifications for the expired hosts

--- src/main/java/com/tddapps/actions/NotificationCalculatorAction.java
+++ src/main/java/com/tddapps/actions/NotificationCalculatorAction.java
@@ -1,12 +1,13 @@
 package com.tddapps.actions;
 
-import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.tddapps.actions.response.TextMessage;
 import com.tddapps.controllers.ActionProcessException;
 import com.tddapps.controllers.HttpJsonResponse;
 import com.tddapps.controllers.HttpSupplierAction;
-import com.tddapps.dal.*;
+import com.tddapps.dal.DalException;
+import com.tddapps.dal.HeartBeat;
+import com.tddapps.dal.HeartBeatRepository;
+import com.tddapps.dal.NotificationSender;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,34 +16,42 @@ import java.util.Arrays;
 public class NotificationCalculatorAction implements HttpSupplierAction<TextMessage> {
     private static final Logger LOG = LogManager.getLogger(NotificationCalculatorAction.class);
     private final HeartBeatRepository heartBeatRepository;
-    private final SettingsReader settingsReader;
+    private final NotificationSender notificationSender;
 
-    public NotificationCalculatorAction(HeartBeatRepository heartBeatRepository, SettingsReader settingsReader) {
+    public NotificationCalculatorAction(HeartBeatRepository heartBeatRepository, NotificationSender notificationSender) {
         this.heartBeatRepository = heartBeatRepository;
-        this.settingsReader = settingsReader;
+        this.notificationSender = notificationSender;
     }
 
     @Override
     public HttpJsonResponse<TextMessage> process() throws ActionProcessException {
         LOG.info("calculating notifications");
-        String topicName = settingsReader.ReadString(Settings.TOPIC_NAME);
-        LOG.info(String.format("topicName: %s", topicName));
-
-        AmazonSNS notificationSender = AmazonSNSClientBuilder.defaultClient();
 
         try {
-            HeartBeat[] heartBeats = heartBeatRepository.All();
+            HeartBeat[] expiredHeartBeats = Arrays.stream(readHeartBeats())
+                    .filter(HeartBeat::isNotTest)
+                    .filter(HeartBeat::isExpired)
+                    .toArray(HeartBeat[]::new);
 
-            if (heartBeats != null) {
-                Arrays.stream(heartBeats).forEach(hb -> {
-                    LOG.info(hb.toString());
-//                    notificationSender.publish(topicName, hb.toString());
-                });
+            for (HeartBeat hb : expiredHeartBeats){
+                String subject = String.format("Host %s missing", hb.getHostId());
+
+                notificationSender.Send(subject, subject);
             }
         } catch (DalException e) {
             throw new ActionProcessException(e.getMessage());
         }
 
         return HttpJsonResponse.Success(TextMessage.OK);
+    }
+
+    private HeartBeat[] readHeartBeats() throws DalException {
+        HeartBeat[] result = heartBeatRepository.All();
+
+        if (result == null){
+            return new HeartBeat[]{};
+        }
+
+        return result;
     }
 }
--- src/test/java/com/tddapps/actions/NotificationCalculatorActionTest.java
+++ src/test/java/com/tddapps/actions/NotificationCalculatorActionTest.java
@@ -4,20 +4,21 @@ import com.tddapps.actions.response.TextMessage;
 import com.tddapps.controllers.ActionProcessException;
 import com.tddapps.controllers.HttpJsonResponse;
 import com.tddapps.dal.DalException;
+import com.tddapps.dal.HeartBeat;
 import com.tddapps.dal.HeartBeatRepository;
-import com.tddapps.dal.SettingsReader;
+import com.tddapps.dal.NotificationSender;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.verification.Times;
 
+import static com.tddapps.utils.DateExtensions.UtcNowPlusMs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class NotificationCalculatorActionTest {
     private final HeartBeatRepository heartBeatRepository = mock(HeartBeatRepository.class);
-    private final SettingsReader settingsReader = mock(SettingsReader.class);
-    private final NotificationCalculatorAction action = new NotificationCalculatorAction(heartBeatRepository, settingsReader);
+    private final NotificationSender notificationSender = mock(NotificationSender.class);
+    private final NotificationCalculatorAction action = new NotificationCalculatorAction(heartBeatRepository, notificationSender);
 
     @Test
     public void ReadsAllTheHeartBeats(){
@@ -46,6 +47,39 @@ public class NotificationCalculatorActionTest {
             assertEquals("All failed", e.getMessage());
         } catch (DalException e) {
             fail("All should not have thrown", e);
+        }
+    }
+
+    @Test
+    public void SendsNotificationsForEachExpiredHeartBeat(){
+        HeartBeat[] seededHeartBeats = new HeartBeat[]{
+                new HeartBeat("hbExpired1", UtcNowPlusMs(-5000)),
+                new HeartBeat("hbExpiredTest1", UtcNowPlusMs(-5000), true),
+                new HeartBeat("hb1", UtcNowPlusMs(5000)),
+                new HeartBeat("hbExpired2", UtcNowPlusMs(-15000)),
+                new HeartBeat("hb2", UtcNowPlusMs(25000))
+        };
+        try {
+            doReturn(seededHeartBeats)
+                    .when(heartBeatRepository)
+                    .All();
+        } catch (DalException e) {
+            fail("All should not have thrown", e);
+        }
+
+        try {
+            action.process();
+
+            verify(notificationSender, times(2))
+                    .Send(any(String.class), any(String.class));
+            verify(notificationSender)
+                    .Send("Host hbExpired1 missing", "Host hbExpired1 missing");
+            verify(notificationSender)
+                    .Send("Host hbExpired2 missing", "Host hbExpired2 missing");
+        } catch (ActionProcessException e) {
+            fail("Process should not have thrown", e);
+        } catch (DalException e) {
+            fail("Send should not have thrown", e);
         }
     }
 }
