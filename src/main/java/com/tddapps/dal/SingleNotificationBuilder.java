e407def8bba10f0fe54473370bc1bbcb14583b96
Camilo Sanchez <camilin87@gmail.com>
2018-09-07 08:55:10 -0400

    refactor the SingleNotificationBuilder

--- src/main/java/com/tddapps/dal/SingleNotificationBuilder.java
+++ src/main/java/com/tddapps/dal/SingleNotificationBuilder.java
@@ -9,23 +9,26 @@ public class SingleNotificationBuilder implements HeartBeatNotificationBuilder {
             return NoNotifications();
         }
 
-        String hostnames = Arrays.stream(heartBeats)
-                .map(HeartBeat::getHostId)
-                .reduce((a, b) -> String.format("%s, %s", a, b))
-                .orElse("");
+        String subject = String.format("Hosts missing [%s]", getHostNames(heartBeats));
+        String message = String.format("%s\n\n%s\n--", subject, getHeartBeatDetails(heartBeats));
 
-        String heartBeatDetails = Arrays.stream(heartBeats)
+        return new Notification[]{
+                new Notification(subject,message)
+        };
+    }
+
+    private String getHeartBeatDetails(HeartBeat[] heartBeats) {
+        return Arrays.stream(heartBeats)
                 .map(HeartBeat::toString)
                 .reduce((a, b) -> String.format("%s\n%s", a, b))
                 .orElse("");
+    }
 
-        String subject = String.format("Hosts missing [%s]", hostnames);
-
-        String message = String.format("%s\n\n%s\n--", subject, heartBeatDetails);
-
-        return new Notification[]{
-                new Notification(subject, message)
-        };
+    private String getHostNames(HeartBeat[] heartBeats) {
+        return Arrays.stream(heartBeats)
+                .map(HeartBeat::getHostId)
+                .reduce((a, b) -> String.format("%s, %s", a, b))
+                .orElse("");
     }
 
     private boolean isEmpty(HeartBeat[] heartBeats) {
