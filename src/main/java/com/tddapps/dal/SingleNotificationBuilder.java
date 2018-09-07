44b5e9b7d960f030d632416c0347d22e2df5f371
Camilo Sanchez <camilin87@gmail.com>
2018-09-07 08:35:49 -0400

    sends a notification with subject for a single heartbeat

--- src/main/java/com/tddapps/dal/SingleNotificationBuilder.java
+++ src/main/java/com/tddapps/dal/SingleNotificationBuilder.java
@@ -3,6 +3,22 @@ package com.tddapps.dal;
 public class SingleNotificationBuilder implements HeartBeatNotificationBuilder {
     @Override
     public Notification[] build(HeartBeat[] heartBeats) {
+        if (isEmpty(heartBeats)){
+            return NoNotifications();
+        }
+
+        String subject = String.format("Hosts missing [%s]", heartBeats[0].getHostId());
+
+        return new Notification[]{
+                new Notification(subject, null)
+        };
+    }
+
+    private boolean isEmpty(HeartBeat[] heartBeats) {
+        return heartBeats == null || heartBeats.length == 0;
+    }
+
+    private Notification[] NoNotifications() {
         return new Notification[0];
     }
 }
--- src/test/java/com/tddapps/dal/SingleNotificationBuilderTest.java
+++ src/test/java/com/tddapps/dal/SingleNotificationBuilderTest.java
@@ -1,6 +1,8 @@
 package com.tddapps.dal;
 
 import org.junit.jupiter.api.Test;
+
+import static com.tddapps.utils.DateExtensions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SingleNotificationBuilderTest {
@@ -10,5 +12,19 @@ public class SingleNotificationBuilderTest {
     public void DoesNotSendNotificationWhenNoHeartBeatsAreProvided(){
         assertEquals(0, builder.build(null).length);
         assertEquals(0, builder.build(new HeartBeat[]{}).length);
+    }
+
+    @Test
+    public void SendsNotificationForASingleHeartBeat(){
+        HeartBeat hb1 = new HeartBeat("host1", UtcNow());
+        HeartBeat[] input = new HeartBeat[]{
+                hb1
+        };
+
+        Notification[] notifications = builder.build(input);
+        assertEquals(1, notifications.length);
+        Notification notification = notifications[0];
+
+        assertEquals("Hosts missing [host1]", notification.getSubject());
     }
 }
