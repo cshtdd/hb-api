7cd21bc6a62cbc04891ea1eaadbc993bae1fb377
Camilo Sanchez <camilin87@gmail.com>
2018-09-10 19:41:33 -0400

    correct the local dynamo url, since the lambdas will run inside a container, they need to reference the docker host as docker.for.mac.localhost

--- dev/run.sh
+++ dev/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 mvn clean install && \
-    sls sam export --output .sam_template.yml --dynamoDbEndpointOverride="http://localhost:8000" && \
+    sls sam export --output .sam_template.yml --dynamoDbEndpointOverride="http://docker.for.mac.localhost:8000" && \
     (trap 'kill 0' SIGINT; \
         sls dynamodb start --stage dev & \
         ( \
