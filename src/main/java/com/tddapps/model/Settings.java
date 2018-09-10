2068c2aeec174bfece905644fdd66872e5e171d1
Camilo Sanchez <camilin87@gmail.com>
2018-09-10 19:42:40 -0400

    the DynamoDBMapperFactoryWithTablePrefix has the ability to point to a local dynamo Instance

--- src/main/java/com/tddapps/model/Settings.java
+++ src/main/java/com/tddapps/model/Settings.java
@@ -3,4 +3,5 @@ package com.tddapps.model;
 public abstract class Settings {
     public final static String TOPIC_NAME = "TOPIC_NAME";
     public final static String TABLE_PREFIX = "TABLE_PREFIX";
+    public final static String DYNAMO_DB_ENDPOINT_OVERRIDE = "DYNAMO_DB_ENDPOINT_OVERRIDE";
 }
--- src/main/java/com/tddapps/model/aws/DynamoDBMapperFactoryWithTablePrefix.java
+++ src/main/java/com/tddapps/model/aws/DynamoDBMapperFactoryWithTablePrefix.java
@@ -1,26 +1,64 @@
 package com.tddapps.model.aws;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.tddapps.model.Settings;
 import com.tddapps.model.SettingsReader;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
 
+@Log4j2
 public class DynamoDBMapperFactoryWithTablePrefix implements DynamoDBMapperFactory {
     private final DynamoDBMapper mapper;
+    private final SettingsReader settingsReader;
 
     public DynamoDBMapperFactoryWithTablePrefix(SettingsReader settingsReader){
-        val tablePrefix = settingsReader.ReadString(Settings.TABLE_PREFIX);
+        this.settingsReader = settingsReader;
 
-        val client = AmazonDynamoDBClientBuilder.defaultClient();
+        mapper = new DynamoDBMapper(
+                getDynamoDbClient(),
+                getConfigBuilder().build()
+        );
+    }
+
+    private DynamoDBMapperConfig.Builder getConfigBuilder() {
+        val tablePrefix = settingsReader.ReadString(Settings.TABLE_PREFIX);
         val tableNameOverride = DynamoDBMapperConfig.TableNameOverride.withTableNamePrefix(tablePrefix);
 
         val configBuilder = DynamoDBMapperConfig.builder();
         configBuilder.setTableNameOverride(tableNameOverride);
-        DynamoDBMapperConfig config = configBuilder.build();
+        return configBuilder;
+    }
 
-        mapper = new DynamoDBMapper(client, config);
+    private AmazonDynamoDB getDynamoDbClient() {
+        val dynamoDbEndpointOverride = settingsReader.ReadString(Settings.DYNAMO_DB_ENDPOINT_OVERRIDE);
+        if (dynamoDbEndpointOverride.isEmpty()){
+            return getDynamoDBClientDefault();
+        }
+
+        return getDynamoDbClientLocal(dynamoDbEndpointOverride);
+    }
+
+    private AmazonDynamoDB getDynamoDBClientDefault() {
+        return AmazonDynamoDBClientBuilder.defaultClient();
+    }
+
+    private AmazonDynamoDB getDynamoDbClientLocal(String dynamoDbEndpointOverride) {
+        log.info(String.format("Override Dynamo DB Endpoint; endpoint: %s", dynamoDbEndpointOverride));
+
+        val endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
+                dynamoDbEndpointOverride,
+                Regions.DEFAULT_REGION.getName()
+        );
+
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(endpointConfiguration)
+                .build();
     }
 
     @Override
