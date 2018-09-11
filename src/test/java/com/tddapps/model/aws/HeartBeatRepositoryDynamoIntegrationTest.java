cf137c0fc94195b75792a4091cbf7df78dbc3c16
Camilo Sanchez <camilin87@gmail.com>
2018-09-11 19:57:14 -0400

    add a characterization test that proves heartbeats can be saved

--- src/test/java/com/tddapps/model/aws/HeartBeatRepositoryDynamoIntegrationTest.java
+++ src/test/java/com/tddapps/model/aws/HeartBeatRepositoryDynamoIntegrationTest.java
@@ -2,10 +2,13 @@ package com.tddapps.model.aws;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.tddapps.model.DalException;
+import com.tddapps.model.HeartBeatFactory;
+import lombok.var;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HeartBeatRepositoryDynamoIntegrationTest {
     private DynamoDBMapper dbMapper = null;
@@ -22,5 +25,16 @@ public class HeartBeatRepositoryDynamoIntegrationTest {
     @Test
     public void ThereAreNoHeartBeatsByDefault() throws DalException {
         assertEquals(0, repository.All().length);
+    }
+
+    @Test
+    public void HeartBeatsCanBeSaved() throws DalException {
+        var hb1 = HeartBeatFactory.create();
+        repository.Save(hb1);
+
+        var heartBeats = repository.All();
+
+        assertEquals(1, heartBeats.length);
+        assertTrue(hb1.almostEquals(heartBeats[0]));
     }
 }
