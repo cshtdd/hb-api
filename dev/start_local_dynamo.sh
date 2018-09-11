e4ecac6bae56a6acb48277e19eae1c00ab36ecf8
Camilo Sanchez <camilin87@gmail.com>
2018-09-11 15:02:13 -0400

    start a logal dynamodb before deployments so that integration tests can be automatically executed

---
+++ dev/start_local_dynamo.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "" ]]; then
+    PORT="$1"
+else
+    PORT="8000"
+fi
+
+ENDPOINT="http://localhost:$PORT"
+echo "Dynamo endpoint: ${ENDPOINT}"
+
+echo "Checking Local Dynamo..."
+LOCAL_DYNAMO_TABLES=`aws dynamodb list-tables --endpoint-url ${ENDPOINT}`
+
+if [[ ${LOCAL_DYNAMO_TABLES} = *"\"TableNames\""* ]]; then
+    echo "Success! Local Dynamo Already Running"
+else
+    echo "Starting Local Dynamo..."
+    sls dynamodb start --stage dev --port ${PORT} &
+    sleep 2
+
+    echo "Checking Local Dynamo..."
+    LOCAL_DYNAMO_TABLES=`aws dynamodb list-tables --endpoint-url ${ENDPOINT}`
+    if [[ ${LOCAL_DYNAMO_TABLES} = *"\"TableNames\""* ]]; then
+        echo "Success! Local Dynamo Running"
+    else
+        echo "Error! Local Dynamo Not Running"
+        exit 1
+    fi
+fi
