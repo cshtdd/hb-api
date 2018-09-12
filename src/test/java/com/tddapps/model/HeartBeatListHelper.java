53a13f5854706bb5ed70e0fbdc05a31c38382bd6
Camilo Sanchez <camilin87@gmail.com>
2018-09-11 23:12:39 -0400

    extract HeartBeatListHelper to assist with HeartBeat list validation in tests

---
+++ src/test/java/com/tddapps/model/HeartBeatListHelper.java
@@ -0,0 +1,23 @@
+package com.tddapps.model;
+
+import lombok.val;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public abstract class HeartBeatListHelper {
+    public static void ShouldMatch(HeartBeat[] listA, HeartBeat[] listB) {
+        assertEquals(listA.length, listB.length);
+
+        Predicate<HeartBeat> listBContainsHeartBeat = hb -> Arrays.stream(listB)
+                .anyMatch(hb::almostEquals);
+
+        val allSeededHeartBeatsHaveBeenRetrieved = Arrays.stream(listA)
+                .allMatch(listBContainsHeartBeat);
+
+        assertTrue(allSeededHeartBeatsHaveBeenRetrieved);
+    }
+}
--- src/test/java/com/tddapps/model/aws/HeartBeatRepositoryDynamoIntegrationTest.java
+++ src/test/java/com/tddapps/model/aws/HeartBeatRepositoryDynamoIntegrationTest.java
@@ -2,18 +2,14 @@ package com.tddapps.model.aws;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.tddapps.model.DalException;
-import com.tddapps.model.HeartBeat;
 import com.tddapps.model.HeartBeatFactory;
+import com.tddapps.model.HeartBeatListHelper;
 import lombok.val;
 import lombok.var;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HeartBeatRepositoryDynamoIntegrationTest {
     private DynamoDBMapper dbMapper = null;
@@ -39,7 +35,7 @@ public class HeartBeatRepositoryDynamoIntegrationTest {
             repository.Save(seededHeartBeat);
         }
 
-        listsMatch(seededHeartBeats, repository.All());
+        HeartBeatListHelper.ShouldMatch(seededHeartBeats, repository.All());
     }
 
     @Test
@@ -48,18 +44,6 @@ public class HeartBeatRepositoryDynamoIntegrationTest {
 
         repository.Save(seededHeartBeats);
 
-        listsMatch(seededHeartBeats, repository.All());
-    }
-
-    private void listsMatch(HeartBeat[] listA, HeartBeat[] listB) {
-        assertEquals(listA.length, listB.length);
-
-        Predicate<HeartBeat> listBContainsHeartBeat = hb -> Arrays.stream(listB)
-                .anyMatch(hb::almostEquals);
-
-        val allSeededHeartBeatsHaveBeenRetrieved = Arrays.stream(listA)
-                .allMatch(listBContainsHeartBeat);
-
-        assertTrue(allSeededHeartBeatsHaveBeenRetrieved);
+        HeartBeatListHelper.ShouldMatch(seededHeartBeats, repository.All());
     }
 }
