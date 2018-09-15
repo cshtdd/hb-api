55155a9d31f33f1a669ccac6928ee406e600e2ae
Camilo Sanchez <camilin87@gmail.com>
2018-09-14 20:42:25 -0400

    optimize usings

--- src/main/java/com/tddapps/infrastructure/InMemoryKeysCacheWithExpiration.java
+++ src/main/java/com/tddapps/infrastructure/InMemoryKeysCacheWithExpiration.java
@@ -7,7 +7,6 @@ import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
-import org.ehcache.expiry.ExpiryPolicy;
 
 import java.io.Closeable;
 import java.io.IOException;
