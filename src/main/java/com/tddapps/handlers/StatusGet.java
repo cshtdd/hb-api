2351d52dd341c84cab05449650b314c94e83de39
Camilo Sanchez <camilin87@gmail.com>
2018-09-17 18:03:45 -0400

    first step towards flattening the StatusGet handler

--- src/main/java/com/tddapps/handlers/StatusGet.java
+++ src/main/java/com/tddapps/handlers/StatusGet.java
@@ -1,17 +1,54 @@
 package com.tddapps.handlers;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.tddapps.actions.StatusGetAction;
-import com.tddapps.controllers.HttpJsonController;
-import com.tddapps.controllers.HttpJsonControllerSupplier;
-import com.tddapps.handlers.infrastructure.BaseHttpJsonHandler;
+import com.tddapps.controllers.ActionProcessException;
+import com.tddapps.controllers.HttpJsonResponse;
+import com.tddapps.handlers.infrastructure.ApiGatewayResponse;
 import com.tddapps.ioc.IocContainer;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
 
+import java.util.Map;
+
 @SuppressWarnings("unused")
-public class StatusGet extends BaseHttpJsonHandler {
-    @Override
-    protected HttpJsonController getController() {
-        val action = IocContainer.getInstance().Resolve(StatusGetAction.class);
-        return new HttpJsonControllerSupplier(action);
+@Log4j2
+public class StatusGet implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+    private final StatusGetAction action;
+
+    public StatusGet(){
+        this(IocContainer.getInstance().Resolve(StatusGetAction.class));
     }
+
+    public StatusGet(StatusGetAction action){
+        this.action = action;
+    }
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        log.debug(String.format("Input: %s", input));
+
+        try {
+            val successResponse = action.process();
+
+            log.debug(String.format("StatusCode: %s, ResponseBody: %s", successResponse.getStatusCode(), successResponse.getBody()));
+
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(successResponse.getStatusCode())
+                    .setObjectBody(successResponse.getBody())
+                    .build();
+
+        } catch (ActionProcessException e) {
+            log.error("Action processing failed", e);
+
+            val errorResponse = HttpJsonResponse.ServerErrorWithMessage(e.getMessage());
+
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(errorResponse.getStatusCode())
+                    .setObjectBody(errorResponse.getBody())
+                    .build();
+        }
+    }
+
 }
--- src/test/java/com/tddapps/handlers/StatusGetTest.java
+++ src/test/java/com/tddapps/handlers/StatusGetTest.java
@@ -1,16 +1,113 @@
 package com.tddapps.handlers;
 
 import com.tddapps.actions.StatusGetAction;
-import com.tddapps.controllers.HttpJsonControllerSupplier;
+import com.tddapps.handlers.infrastructure.ApiGatewayResponse;
+import com.tddapps.infrastructure.KeysCacheStub;
+import com.tddapps.model.DalException;
+import com.tddapps.model.HeartBeat;
+import com.tddapps.model.HeartBeatRepository;
+import com.tddapps.model.NotificationSenderStatus;
 import lombok.val;
 import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.tddapps.utils.DateExtensions.UtcNowPlusMs;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
 
 public class StatusGetTest {
-    @Test
-    public void BuildsTheCorrectController(){
-        val controller = (HttpJsonControllerSupplier) new StatusGet().getController();
-        assertTrue(controller.getAction() instanceof StatusGetAction);
+    private final HeartBeatRepository heartBeatRepository = mock(HeartBeatRepository.class);
+    private final NotificationSenderStatus notificationSenderStatus = mock(NotificationSenderStatus.class);
+    private final KeysCacheStub keysCache = new KeysCacheStub();
+    private final StatusGet handler = new StatusGet(new StatusGetAction(heartBeatRepository, notificationSenderStatus, keysCache));
+
+    private ApiGatewayResponse handleRequest() {
+        return handler.handleRequest(new HashMap<>(), null);
     }
+
+    @Test
+    public void VerifiesHeartBeatsCanBeSaved() throws DalException {
+        val expectedHeartBeat = new HeartBeat(
+                "StatusGetAction",
+                UtcNowPlusMs(4*60*60*1000),
+                true
+        );
+
+
+        val result = handleRequest();
+
+
+        assertEquals(200, result.getStatusCode());
+        verify(heartBeatRepository).Save(argThat(expectedHeartBeat::almostEquals));
+    }
+
+    @Test
+    public void ProcessThrowsAnActionProcessExceptionWhenTheHeartBeatCouldNotBeSaved() throws DalException {
+        doThrow(new DalException("Save failed"))
+                .when(heartBeatRepository)
+                .Save(any(HeartBeat.class));
+
+        val result = handleRequest();
+
+        assertEquals(500, result.getStatusCode());
+        assertEquals("{\"message\":\"Save failed\"}", result.getBody());
+    }
+
+    @Test
+    public void VerifiesNotificationsCanBeSent() throws DalException{
+        handleRequest();
+
+        verify(notificationSenderStatus).Verify();
+    }
+
+    @Test
+    public void ProcessThrowsAnActionProcessExceptionWhenTheNotificationsCannotBeSent() throws DalException {
+        doThrow(new DalException("Sent Notifications Fail"))
+                .when(notificationSenderStatus)
+                .Verify();
+
+        val result = handleRequest();
+
+        assertEquals(500, result.getStatusCode());
+        assertEquals("{\"message\":\"Sent Notifications Fail\"}", result.getBody());
+    }
+
+    @Test
+    public void ProcessCachesResult() throws DalException {
+        final List<InvocationOnMock> invocations = new ArrayList<>();
+        doAnswer(invocations::add)
+                .when(heartBeatRepository)
+                .Save(any(HeartBeat.class));
+
+        handleRequest();
+        handleRequest();
+
+        assertTrue(keysCache.Contains(StatusGetAction.class.getName()));
+        assertEquals(1, invocations.size());
+
+        keysCache.getKeys().clear();
+        handleRequest();
+
+        assertTrue(keysCache.Contains(StatusGetAction.class.getName()));
+        assertEquals(2, invocations.size());
+    }
+
+    @Test
+    public void ProcessDoesNotCacheFailures() throws DalException {
+        doThrow(new DalException("Save failed"))
+                .when(heartBeatRepository)
+                .Save(any(HeartBeat.class));
+
+        handleRequest();
+
+        assertFalse(keysCache.Contains(StatusGetAction.class.getName()));
+    }
+
 }
