6d39ca1c86e5f3491a19e21c61729ec2ff7fafe8
Camilo Sanchez <camilin87@gmail.com>
2018-09-18 09:16:43 -0400

    the HeartBeatChange sends notifications for the correct hosts

--- src/main/java/com/tddapps/handlers/HeartBeatChange.java
+++ src/main/java/com/tddapps/handlers/HeartBeatChange.java
@@ -1,12 +1,19 @@
 package com.tddapps.handlers;
 
+import com.amazonaws.services.dynamodbv2.model.Record;
+import com.amazonaws.services.dynamodbv2.model.StreamRecord;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.tddapps.ioc.IocContainer;
+import com.tddapps.model.DalException;
+import com.tddapps.model.Notification;
 import com.tddapps.model.NotificationSender;
 import lombok.extern.log4j.Log4j2;
 import lombok.val;
+import lombok.var;
+
+import java.util.Arrays;
 
 @Log4j2
 @SuppressWarnings("unused")
@@ -23,9 +30,24 @@ public class HeartBeatChange implements RequestHandler<DynamodbEvent, Boolean> {
 
     @Override
     public Boolean handleRequest(DynamodbEvent input, Context context) {
+        val deletedHostIds = input.getRecords()
+                .stream()
+                .filter(r -> r.getEventName().equals("REMOVE"))
+                .map(Record::getDynamodb)
+                .map(StreamRecord::getKeys)
+                .map(k -> k.get("host_id").toString())
+                .toArray(String[]::new);
 
-        for (val r : input.getRecords()) {
-            log.info(String.format("EventName: %s, Key: %s", r.getEventName(), r.getDynamodb().getKeys().get("host_id")));
+        val notifications = Arrays.stream(deletedHostIds)
+                .map(hostId -> new Notification("", ""))
+                .toArray(Notification[]::new);
+
+        for (val n : notifications){
+            try {
+                notificationSender.Send(n);
+            } catch (DalException e) {
+                //TODO handle this better
+            }
         }
 
         return true;
--- src/test/java/com/tddapps/handlers/HeartBeatChangeTest.java
+++ src/test/java/com/tddapps/handlers/HeartBeatChangeTest.java
@@ -1,17 +1,73 @@
 package com.tddapps.handlers;
 
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.StreamRecord;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.tddapps.model.DalException;
+import com.tddapps.model.Notification;
 import com.tddapps.model.NotificationSender;
+import lombok.Data;
+import lombok.val;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class HeartBeatChangeTest {
     private final NotificationSender notificationSender = mock(NotificationSender.class);
     private final HeartBeatChange handler = new HeartBeatChange(notificationSender);
 
+    @Data
+    private static class HeartBeatEvent{
+        final String name;
+        final String hostId;
+    }
+
     @Test
     public void CanBeConstructedUsingTheDefaultConstructor(){
         assertNotNull(new HeartBeatChange());
+    }
+
+    @Test
+    public void SendsANotificationForEachDeletedRecord() throws DalException {
+        val result = handleRequest(
+                new HeartBeatEvent("MODIFY", "host1"),
+                new HeartBeatEvent("INSERT", "host2"),
+                new HeartBeatEvent("REMOVE", "host3"),
+                new HeartBeatEvent("REMOVE", "host4"),
+                new HeartBeatEvent("INSERT", "host5")
+        );
+
+        assertTrue(result);
+        verify(notificationSender, times(2))
+                .Send(any(Notification.class));
+    }
+
+    private boolean handleRequest(HeartBeatEvent ... seededEvents){
+        List<DynamodbEvent.DynamodbStreamRecord> seededRecords = Arrays.stream(seededEvents)
+                .map(e -> {
+                    val d = new StreamRecord();
+                    d.setKeys(new HashMap<String, AttributeValue>() {{
+                        put("host_id", new AttributeValue(e.getHostId()));
+                    }});
+
+                    val result = new DynamodbEvent.DynamodbStreamRecord();
+                    result.setEventName(e.getName());
+                    result.setDynamodb(d);
+
+                    return result;
+                })
+                .collect(Collectors.toList());
+
+        val input = new DynamodbEvent();
+        input.setRecords(seededRecords);
+
+        return handler.handleRequest(input, null);
     }
 }
