bb6e82ddc5925255852402f7dcd7a53db40a332e
Camilo Sanchez <camilin87@gmail.com>
2018-09-17 21:43:08 -0400

    first step towards flattening the NotificationCalculator

--- src/main/java/com/tddapps/handlers/NotificationCalculator.java
+++ src/main/java/com/tddapps/handlers/NotificationCalculator.java
@@ -1,17 +1,31 @@
 package com.tddapps.handlers;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.tddapps.actions.NotificationCalculatorAction;
-import com.tddapps.controllers.HttpJsonController;
-import com.tddapps.controllers.HttpJsonControllerSupplier;
-import com.tddapps.handlers.infrastructure.BaseHttpJsonHandler;
+import com.tddapps.controllers.ActionProcessException;
 import com.tddapps.ioc.IocContainer;
 import lombok.val;
 
 @SuppressWarnings("unused")
-public class NotificationCalculator extends BaseHttpJsonHandler {
+public class NotificationCalculator implements RequestHandler<Boolean, Boolean> {
+    private final NotificationCalculatorAction action;
+
+    public NotificationCalculator(){
+        this(IocContainer.getInstance().Resolve(NotificationCalculatorAction.class));
+    }
+
+    public NotificationCalculator(NotificationCalculatorAction action) {
+        this.action = action;
+    }
+
     @Override
-    protected HttpJsonController getController() {
-        val action = IocContainer.getInstance().Resolve(NotificationCalculatorAction.class);
-        return new HttpJsonControllerSupplier(action);
+    public Boolean handleRequest(Boolean input, Context context) {
+        try {
+            val process = action.process();
+            return true;
+        } catch (ActionProcessException e) {
+            return false;
+        }
     }
 }
