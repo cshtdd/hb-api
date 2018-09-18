2576d16c2443b87f0cc9a7e2d35f77351c63e67e
Camilo Sanchez <camilin87@gmail.com>
2018-09-18 07:32:23 -0400

    remove redundant parsing tests from the HeartBeatPost

--- src/test/java/com/tddapps/handlers/HeartBeatPostTest.java
+++ src/test/java/com/tddapps/handlers/HeartBeatPostTest.java
@@ -21,22 +21,8 @@ import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.*;
 
 public class HeartBeatPostTest {
-    private final List<InvocationOnMock> saveInvocations = new ArrayList<>();
     private final HeartBeatRepository heartBeatRepository = mock(HeartBeatRepository.class);
     private final HeartBeatPost handler = new HeartBeatPost(heartBeatRepository);
-
-    private final String MAXIMUM_LENGTH_ALLOWED_STRING = StringUtils.leftPad("", 100, "0");
-    private final String INVALID_HOST_ID = "Invalid hostId";
-    private final String INVALID_INTERVAL_MS = "Invalid intervalMs";
-
-    @BeforeEach
-    public void Setup(){
-        try {
-            doAnswer(saveInvocations::add)
-                    .when(heartBeatRepository)
-                    .Save(any(HeartBeat.class));
-        } catch (DalException e) { }
-    }
 
     @Test
     public void CanBeConstructedUsingADefaultConstructor(){
@@ -44,78 +30,12 @@ public class HeartBeatPostTest {
     }
 
     @Test
-    public void ReadsTheMaximumLengthHostId() {
-        handleRequest(String.format(
-                "{\"hostId\": \"%s\"}", MAXIMUM_LENGTH_ALLOWED_STRING
-        ));
+    public void ReturnsClientErrorWhenInputIsInvalid() throws DalException{
+        val result = handleRequest("{incorrect json");
 
-        assertEquals(MAXIMUM_LENGTH_ALLOWED_STRING, ((HeartBeat) saveInvocations.get(0).getArgument(0)).getHostId());
-    }
-
-    @Test
-    public void ParsingFailsWhenHostIdIsMissing(){
-        parseShouldFailWithError("{}", INVALID_HOST_ID);
-        parseShouldFailWithError("{\"hostId\": \"\"}", INVALID_HOST_ID);
-        parseShouldFailWithError("{\"hostId\": \"   \"}", INVALID_HOST_ID);
-    }
-
-    @Test
-    public void ParsingFailsWhenHostIdIsNotAlphanumeric(){
-        parseShouldFailWithError("{\"hostId\": \"-!@#$$%^%^ &^&\"}", INVALID_HOST_ID);
-    }
-
-    @Test
-    public void ParsingFailsWhenHostIdIsTooLong(){
-        parseShouldFailWithError(String.format(
-                "{\"hostId\": \"X%s\"}", MAXIMUM_LENGTH_ALLOWED_STRING
-        ), INVALID_HOST_ID);
-    }
-
-    @Test
-    public void ParsingSupportsMultipleDataTypesForIntervalMs() {
-        val expectedHeartBeat = new HeartBeat(
-                "superHost1",
-                UtcNowPlusMs(3000),
-                false
-        );
-
-        handleRequest("{\"hostId\": \"superHost1\", \"intervalMs\": 3000}");
-        handleRequest("{\"hostId\": \"superHost1\", \"intervalMs\": 3000.45}");
-        handleRequest("{\"hostId\": \"superHost1\", \"intervalMs\": \"3000\"}");
-
-        boolean allHeartBeatHaveDefaultExpiration = saveInvocations
-                .stream()
-                .map(i -> (HeartBeat) i.getArgument(0))
-                .allMatch(expectedHeartBeat::almostEquals);
-        assertTrue(allHeartBeatHaveDefaultExpiration);
-    }
-
-    @Test
-    public void ParsingAssumesDefaultWhenIntervalMsIsNotNumeric() {
-        val expectedHeartBeat = new HeartBeat(
-                "superHost1",
-                UtcNowPlusMs(HeartBeat.DEFAULT_INTERVAL_MS),
-                false
-        );
-
-        handleRequest("{\"hostId\": \"superHost1\", \"intervalMs\": null}");
-        handleRequest("{\"hostId\": \"superHost1\", \"intervalMs\": \"\"}");
-        handleRequest("{\"hostId\": \"superHost1\", \"intervalMs\": \" \"}");
-        handleRequest("{\"hostId\": \"superHost1\", \"intervalMs\": \"pete\"}");
-
-        boolean allHeartBeatHaveDefaultExpiration = saveInvocations
-                .stream()
-                .map(i -> (HeartBeat) i.getArgument(0))
-                .allMatch(expectedHeartBeat::almostEquals);
-        assertTrue(allHeartBeatHaveDefaultExpiration);
-    }
-
-    @Test
-    public void ParsingFailsWhenIntervalMsIsOutOfBoundaries(){
-        parseShouldFailWithError("{\"hostId\": \"host1\", \"intervalMs\": 999}", INVALID_INTERVAL_MS);
-        parseShouldFailWithError("{\"hostId\": \"host1\", \"intervalMs\": \"999\"}", INVALID_INTERVAL_MS);
-        parseShouldFailWithError("{\"hostId\": \"host1\", \"intervalMs\": 43200001}", INVALID_INTERVAL_MS);
-        parseShouldFailWithError("{\"hostId\": \"host1\", \"intervalMs\": \"43200001\"}", INVALID_INTERVAL_MS);
+        assertEquals(400, result.getStatusCode());
+        assertEquals("{\"message\":\"Invalid json\"}", result.getBody());
+        verify(heartBeatRepository, times(0)).Save(any(HeartBeat.class));
     }
 
     @Test
@@ -143,15 +63,6 @@ public class HeartBeatPostTest {
 
         assertEquals(500, result.getStatusCode());
         assertEquals("{\"message\":\"Save failed\"}", result.getBody());
-    }
-
-    private void parseShouldFailWithError(String requestBody, String errorMessage){
-        val response = handleRequest(requestBody);
-
-        assertEquals(400, response.getStatusCode());
-
-        val expectedBody = String.format("{\"message\":\"%s\"}", errorMessage);
-        assertEquals(expectedBody, response.getBody());
     }
 
     private ApiGatewayResponse handleRequest(String body) {
