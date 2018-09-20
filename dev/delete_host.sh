b5f209989f07e1ad41d2d623b5d11afa41da3ff6
Camilo Sanchez <camilin87@gmail.com>
2018-09-20 17:43:52 -0400

    provide the option to delete the host without setting it to test first this shall trigger a notification

--- dev/delete_host.sh
+++ dev/delete_host.sh
@@ -2,15 +2,16 @@
 
 HOST_ID=$1
 TABLE_NAME=$2
+SET_TEST_BEFORE_DELETION=1
 
 if [[ $1 ]]; then
     HOST_ID=$1
 else
-    echo "Error: missing hostId"
+    echo "ERROR: missing hostId"
     echo ""
     echo "Usage:"
     echo "------"
-    echo "delete_host.sh <hostId> [dynamoTableName]"
+    echo "delete_host.sh <hostId> [dynamoTableName] [--skip-set-test]"
     exit 1
 fi
 
@@ -20,7 +21,11 @@ else
     TABLE_NAME="hb-api-dev-heartbeats"
 fi
 
-echo "Deleting hostId:$HOST_ID from tableName:$TABLE_NAME"
+if [[ "$3" == "--skip-set-test" ]]; then
+    SET_TEST_BEFORE_DELETION=0
+fi
+
+echo "INFO: Deleting hostId:$HOST_ID from tableName:$TABLE_NAME"
 
 TMP_KEY_FILE=$(mktemp)
 bash -c "cat <<EOF > ${TMP_KEY_FILE}
@@ -34,10 +39,12 @@ bash -c "cat <<EOF > ${TMP_VALUE_FILE}
 EOF"
 # echo "DEBUG: TMP_VALUE_FILE=${TMP_VALUE_FILE}\n`cat ${TMP_VALUE_FILE}`\n"
 
-# echo "DEBUG: updating test attribute"
-aws dynamodb update-item --table-name "${TABLE_NAME}" \
-  --key file://${TMP_KEY_FILE} --update-expression "SET test = :t" \
-  --expression-attribute-values file://$TMP_VALUE_FILE
+if [[ $SET_TEST_BEFORE_DELETION -eq 1 ]]; then
+  # echo "DEBUG: updating test attribute"
+  aws dynamodb update-item --table-name "${TABLE_NAME}" \
+    --key file://${TMP_KEY_FILE} --update-expression "SET test = :t" \
+    --expression-attribute-values file://$TMP_VALUE_FILE
+fi
 
 # echo "DEBUG: deleting item"
 aws dynamodb delete-item --table-name "${TABLE_NAME}" --key file://${TMP_KEY_FILE}
