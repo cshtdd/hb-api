dd142f04684b0be552e64d1d3c4ce07587a737a0
Camilo Sanchez <camilin87@gmail.com>
2018-09-22 11:18:15 -0400

    delete subscription is region-aware

--- dev/delete_subscription.sh
+++ dev/delete_subscription.sh
@@ -1,40 +1,51 @@
 #!/usr/bin/bash
 
 INPUT_ENDPOINT=$1
-TOPIC_NAME=$2
+REGION=$2
+TOPIC_NAME="hb-api-dev-heartbeat-notifications"
 
-if [[ $1 ]]; then
-    INPUT_ENDPOINT=$1
-else
+if [[ -z $1 ]]; then
     echo "ERROR: missing endpoint"
     echo ""
     echo "Usage:"
     echo "------"
-    echo "delete_subscription.sh <endpoint> [snsTopicName]"
+    echo "delete_subscription.sh <endpoint> <region> [snsTopicName]"
     echo "------"
     echo "example 1:"
-    echo "delete_subscription.sh test@test.com my-topic"
+    echo "delete_subscription.sh test@test.com us-east-1 my-topic"
     echo "example 2:"
-    echo "delete_subscription.sh +17861234567 my-topic"
+    echo "delete_subscription.sh +17861234567 us-east-1 my-topic"
     exit 1
 fi
 
-if [[ $2 ]]; then
-    TOPIC_NAME=$2
-else
-    TOPIC_NAME="hb-api-dev-heartbeat-notifications"
+if [[ -z $2 ]]; then
+    echo "ERROR: missing region"
+    echo ""
+    echo "Usage:"
+    echo "------"
+    echo "delete_subscription.sh <endpoint> <region> [snsTopicName]"
+    echo "------"
+    echo "example 1:"
+    echo "delete_subscription.sh test@test.com us-east-1 my-topic"
+    echo "example 2:"
+    echo "delete_subscription.sh +17861234567 us-east-1 my-topic"
+    exit 1
 fi
 
-echo "INFO: Deleting endpoint:${INPUT_ENDPOINT} from topic:${TOPIC_NAME}"
+if [[ $3 ]]; then
+    TOPIC_NAME=$3
+fi
 
-TOPIC_ARN=$(aws sns list-topics | jq -r ".Topics[].TopicArn | select(. | contains(\":${TOPIC_NAME}\"))")
+echo "INFO: Deleting endpoint:${INPUT_ENDPOINT} from topic:${TOPIC_NAME}[${REGION}]"
+
+TOPIC_ARN=$(aws sns list-topics --region ${REGION} | jq -r ".Topics[].TopicArn | select(. | contains(\":${TOPIC_NAME}\"))")
 if [[ "${TOPIC_ARN}" == "" ]]; then
     echo "ERROR: topic not found"
     exit 1
 fi
 
-SUBSCRIPTIONS_BY_TOPIC=$(aws sns list-subscriptions-by-topic --topic-arn ${TOPIC_ARN})
+SUBSCRIPTIONS_BY_TOPIC=$(aws sns list-subscriptions-by-topic --topic-arn ${TOPIC_ARN} --region ${REGION})
 SUBSCRIPTION_ARN=$(echo $SUBSCRIPTIONS_BY_TOPIC | jq -r ".Subscriptions[] | select(.Endpoint==\"${INPUT_ENDPOINT}\") | .SubscriptionArn")
 echo "DEBUG: SubscriptionArn=${SUBSCRIPTION_ARN}"
 
-aws sns unsubscribe --subscription-arn "${SUBSCRIPTION_ARN}"
+aws sns unsubscribe --subscription-arn "${SUBSCRIPTION_ARN}" --region ${REGION}
