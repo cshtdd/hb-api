bdd56a4be5df4a84ab28e6ed2ddf992ac08299fa
Camilo Sanchez <camilin87@gmail.com>
2018-09-22 12:21:00 -0400

    make infrastructure parameters have default values for local execution

--- dev/run.sh
+++ dev/run.sh
@@ -7,9 +7,9 @@ else
 fi
 
 mvn clean install -DskipTests && \
-    sls sam export --region us-east-1 --stage dev --domainName hbapidev.tddapps.com --output .sam_template.yml --dynamoDbEndpointOverride="http://docker.for.mac.localhost:8000" && \
+    sls sam export --output .sam_template.yml --dynamoDbEndpointOverride="http://docker.for.mac.localhost:8000" && \
     (trap 'kill 0' SIGINT; \
-        sls dynamodb start --region us-east-1 --stage dev --domainName hbapidev.tddapps.com & \
+        sls dynamodb start --stage dev & \
         ( \
             sam local start-api ${DEBUG_INFO} -t .sam_template.yml \
         ) \
