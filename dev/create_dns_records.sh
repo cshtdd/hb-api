5cb051c55d899a4c6e59ae4745c8c112fa894d7e
Camilo Sanchez <camilin87@gmail.com>
2018-09-27 12:56:26 -0400

    cleanup command output

--- dev/create_dns_records.sh
+++ dev/create_dns_records.sh
@@ -40,18 +40,18 @@ DOMAIN_NAME=${SUBDOMAIN}.${TLD}
 echo "INFO: Creating domain records ${DOMAIN_NAME} in ${STACKNAME}[${REGION}]"
 
 HOSTEDZONE=$(aws route53 list-hosted-zones --query 'HostedZones[?Name==`'${TLD}'.`].Id' --output text)
-echo "DEBUG: hostedZone: ${HOSTEDZONE}"
+# echo "DEBUG: hostedZone: ${HOSTEDZONE}"
 
 DNS_DOMAIN=$(aws cloudformation describe-stacks --stack-name ${STACKNAME} --region ${REGION} --query 'Stacks[0].Outputs[?OutputKey==`DomainName`].OutputValue' --output text)
-echo "DEBUG: domain: ${DNS_DOMAIN}"
+# echo "DEBUG: domain: ${DNS_DOMAIN}"
 
 SERVICE_ENDPOINT=$(aws cloudformation describe-stacks --stack-name ${STACKNAME} --region ${REGION} --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpoint`].OutputValue' --output text)
-echo "DEBUG: endpoint: ${SERVICE_ENDPOINT}"
-# https://stackoverflow.com/questions/2497215/extract-domain-name-from-url
+# echo "DEBUG: endpoint: ${SERVICE_ENDPOINT}"
+# # https://stackoverflow.com/questions/2497215/extract-domain-name-from-url
 HEALTH_CHECK_DOMAIN=$(echo $SERVICE_ENDPOINT | awk -F[/:] '{print $4}')
-echo "DEBUG: healthCheckDomain: ${HEALTH_CHECK_DOMAIN}"
+# echo "DEBUG: healthCheckDomain: ${HEALTH_CHECK_DOMAIN}"
 HEALTH_CHECK_ID=$(aws route53 list-health-checks --region ${REGION} --output text --query 'HealthChecks[?HealthCheckConfig.FullyQualifiedDomainName==`'${HEALTH_CHECK_DOMAIN}'`].Id')
-echo "DEBUG: existing healthCheckId: ${HEALTH_CHECK_ID}"
+# echo "DEBUG: existing healthCheckId: ${HEALTH_CHECK_ID}"
 
 aws route53 change-resource-record-sets \
   --hosted-zone-id ${HOSTEDZONE} \
