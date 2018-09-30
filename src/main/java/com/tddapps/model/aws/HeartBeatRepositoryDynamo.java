b31c9f95bdb3b9437f9967a945b0ae1caabd9f33
Camilo Sanchez <camilin87@gmail.com>
2018-09-30 12:09:19 -0400

    the HeartBeatRepositoryDynamo can find HeartBeats that expire at a specific minute that are older than a specific epoch second

--- src/main/java/com/tddapps/model/aws/HeartBeatRepositoryDynamo.java
+++ src/main/java/com/tddapps/model/aws/HeartBeatRepositoryDynamo.java
@@ -2,7 +2,9 @@ package com.tddapps.model.aws;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.tddapps.model.DalException;
 import com.tddapps.model.HeartBeat;
 import com.tddapps.model.HeartBeatRepository;
@@ -11,6 +13,7 @@ import lombok.val;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import static com.tddapps.utils.ArrayBatchExtensions.Split;
 
@@ -62,5 +65,30 @@ public class HeartBeatRepositoryDynamo implements HeartBeatRepository {
         return mapper
                 .scan(HeartBeat.class, new DynamoDBScanExpression())
                 .toArray(new HeartBeat[0]);
+    }
+
+    public HeartBeat[] ReadOlderThan(String expirationMinuteUtc, long ttl, int maxCount) throws DalException {
+        try {
+            val query = new DynamoDBQueryExpression<HeartBeat>()
+                    .withLimit(maxCount)
+                    .withIndexName("ExpirationMinuteIndex")
+                    .withConsistentRead(false)
+                    .withKeyConditionExpression("#name1 = :val1 AND #name2 < :val2")
+                    .withExpressionAttributeNames(new HashMap<String, String>(){{
+                        put("#name1", "expiration_minute_utc");
+                        put("#name2", "ttl");
+                    }})
+                    .withExpressionAttributeValues(new HashMap<String, AttributeValue>(){{
+                        put(":val1", new AttributeValue().withS(expirationMinuteUtc));
+                        put(":val2", new AttributeValue().withN(String.format("%d", ttl)));
+                    }});
+
+            return mapper.queryPage(HeartBeat.class, query)
+                    .getResults()
+                    .toArray(new HeartBeat[0]);
+        } catch (AmazonClientException e){
+            log.debug("HeartBeat All Error", e);
+            throw new DalException(e.getMessage());
+        }
     }
 }
--- src/test/java/com/tddapps/model/aws/HeartBeatRepositoryDynamoIntegrationTest.java
+++ src/test/java/com/tddapps/model/aws/HeartBeatRepositoryDynamoIntegrationTest.java
@@ -2,12 +2,17 @@ package com.tddapps.model.aws;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.tddapps.model.DalException;
+import com.tddapps.model.HeartBeat;
 import com.tddapps.model.HeartBeatFactory;
 import com.tddapps.model.HeartBeatListHelper;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static com.tddapps.utils.DateExtensions.ToReverseUtcMinuteString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HeartBeatRepositoryDynamoIntegrationTest {
@@ -45,4 +50,57 @@ public class HeartBeatRepositoryDynamoIntegrationTest {
 
         HeartBeatListHelper.ShouldMatch(seededHeartBeats, repository.All());
     }
+
+
+    @Test
+    public void ReadsHeartBeatsWithTTLInThePast() throws DalException {
+        // 2097-07-17T20:05:31Z[UTC]
+        val seededDate = ZonedDateTime.of(
+                2097, 7, 17,
+                20, 5, 31,
+                45000000,
+                ZoneId.of("UTC")
+        );
+
+        val ttlNow = seededDate.toInstant().getEpochSecond();
+        val expirationMinuteUtcNow = ToReverseUtcMinuteString(ttlNow);
+        val ttlOneSecondAgo = ttlNow - 1;
+        val ttlTenSecondsAgo = ttlNow - 10;
+        val ttlMoreThanOneMinuteAgo = ttlNow - 80;
+        val ttlFuture = ttlNow + 1;
+        val seededHeartBeats = new HeartBeat[]{
+                new HeartBeat("host-00", ttlOneSecondAgo, "us-test-1", false),
+                new HeartBeat("host-01", ttlOneSecondAgo - 1, "us-test-2", false),
+                new HeartBeat("host-02", ttlOneSecondAgo - 2, "us-test-2", false),
+                new HeartBeat("host-03", ttlOneSecondAgo - 3, "us-test-2", false),
+                new HeartBeat("host-04", ttlOneSecondAgo - 4, "us-test-1", false),
+                new HeartBeat("host-05", ttlOneSecondAgo - 5, "us-test-1", false),
+                new HeartBeat("host-06", ttlFuture, "us-test-1", false),
+                new HeartBeat("host-07", ttlFuture, "us-test-1", false),
+                new HeartBeat("host-08", ttlTenSecondsAgo, "us-test-1", false),
+                new HeartBeat("host-09", ttlTenSecondsAgo - 1, "us-test-1", false),
+                new HeartBeat("host-10", ttlTenSecondsAgo - 2, "us-test-1", false),
+                new HeartBeat("host-11", ttlTenSecondsAgo - 3, "us-test-2", false),
+                new HeartBeat("host-12", ttlTenSecondsAgo - 4, "us-test-2", false),
+                new HeartBeat("host-13", ttlTenSecondsAgo - 5, "us-test-1", false),
+                new HeartBeat("host-14", ttlMoreThanOneMinuteAgo, "us-test-1", false),
+                new HeartBeat("host-15", ttlMoreThanOneMinuteAgo, "us-test-1", false),
+                new HeartBeat("host-16", ttlMoreThanOneMinuteAgo, "us-test-1", false),
+                new HeartBeat("host-17", ttlMoreThanOneMinuteAgo, "us-test-1", false),
+                new HeartBeat("host-18", ttlMoreThanOneMinuteAgo, "us-test-1", false),
+        };
+        val expected = new HeartBeat[]{
+                seededHeartBeats[9],
+                seededHeartBeats[10],
+                seededHeartBeats[11],
+                seededHeartBeats[12],
+                seededHeartBeats[13],
+        };
+        repository.Save(seededHeartBeats);
+
+        val actual = repository.ReadOlderThan(expirationMinuteUtcNow, ttlNow, 5);
+
+        HeartBeatListHelper.ShouldMatch(expected, actual);
+    }
+
 }
