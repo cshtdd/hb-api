df4ee3411f122ae6a3a35f1c6bb99d01265e583a
Camilo Sanchez <camilin87@gmail.com>
2018-10-01 07:58:21 -0400

    start defining the HeartBeatExpirator dependencies

--- src/main/java/com/tddapps/handlers/HeartBeatExpirator.java
+++ src/main/java/com/tddapps/handlers/HeartBeatExpirator.java
@@ -2,8 +2,26 @@ package com.tddapps.handlers;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.tddapps.ioc.IocContainer;
+import com.tddapps.model.HeartBeatRepository;
+import com.tddapps.model.SettingsReader;
 
 public class HeartBeatExpirator implements RequestHandler<String, Boolean> {
+    private final HeartBeatRepository heartBeatRepository;
+    private final SettingsReader settingsReader;
+
+    public HeartBeatExpirator(){
+        this(
+                IocContainer.getInstance().Resolve(HeartBeatRepository.class),
+                IocContainer.getInstance().Resolve(SettingsReader.class)
+        );
+    }
+
+    public HeartBeatExpirator(HeartBeatRepository heartBeatRepository, SettingsReader settingsReader) {
+        this.heartBeatRepository = heartBeatRepository;
+        this.settingsReader = settingsReader;
+    }
+
     @Override
     public Boolean handleRequest(String input, Context context) {
         return null;
--- src/test/java/com/tddapps/handlers/HeartBeatExpiratorTest.java
+++ src/test/java/com/tddapps/handlers/HeartBeatExpiratorTest.java
@@ -1,12 +1,29 @@
 package com.tddapps.handlers;
 
+import com.tddapps.model.HeartBeatRepository;
+import com.tddapps.model.Settings;
+import com.tddapps.model.SettingsReader;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class HeartBeatExpiratorTest {
+    private final HeartBeatRepository heartBeatRepository = mock(HeartBeatRepository.class);
+    private final SettingsReader settingsReader = mock(SettingsReader.class);
+    private final HeartBeatExpirator handler = new HeartBeatExpirator(heartBeatRepository, settingsReader);
+
+    @BeforeEach
+    public void Setup(){
+        when(settingsReader.ReadString(Settings.AWS_REGION)).thenReturn("us-test-1");
+    }
+
     @Test
     public void CanBeConstructedUsingTheDefaultConstructor(){
         assertNotNull(new HeartBeatExpirator());
     }
+
+
 }
