679e45bfff60818cc5cb3700d286b6ac49b3211c
Camilo Sanchez <camilin87@gmail.com>
2018-10-01 11:29:12 -0400

    first basic implementation of the RequestHandlerHelperCurrentRegion

--- src/main/java/com/tddapps/model/RequestHandlerHelperCurrentRegion.java
+++ src/main/java/com/tddapps/model/RequestHandlerHelperCurrentRegion.java
@@ -1,4 +1,5 @@
 package com.tddapps.model;
+import java.util.Arrays;
 
 public class RequestHandlerHelperCurrentRegion implements RequestHandlerHelper {
     private final SettingsReader settingsReader;
@@ -9,6 +10,20 @@ public class RequestHandlerHelperCurrentRegion implements RequestHandlerHelper {
 
     @Override
     public HeartBeat[] filter(HeartBeat[] heartBeats) {
-        return new HeartBeat[0];
+        return readHeartBeatsFromCurrentRegion(heartBeats);
+    }
+
+    private HeartBeat[] readHeartBeatsFromCurrentRegion(HeartBeat[] heartBeats){
+        return Arrays.stream(heartBeats)
+                .filter(this::heartBeatLastUpdatedInCurrentRegion)
+                .toArray(HeartBeat[]::new);
+    }
+
+    private boolean heartBeatLastUpdatedInCurrentRegion(HeartBeat hb){
+        return ReadRegion().equals(hb.getRegion());
+    }
+
+    private String ReadRegion() {
+        return settingsReader.ReadString(Settings.AWS_REGION);
     }
 }
--- src/test/java/com/tddapps/model/RequestHandlerHelperCurrentRegionTest.java
+++ src/test/java/com/tddapps/model/RequestHandlerHelperCurrentRegionTest.java
@@ -1,9 +1,13 @@
 package com.tddapps.model;
 
+import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static com.tddapps.model.HeartBeatFactory.TEST_REGION_DEFAULT;
+import static com.tddapps.model.HeartBeatListTestHelper.ShouldMatch;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -18,7 +22,19 @@ public class RequestHandlerHelperCurrentRegionTest {
     }
 
     @Test
-    public void ReturnsAnEmptyListWhenGivenAnEmptyList(){
+    public void FilterReturnsAnEmptyListWhenGivenAnEmptyList(){
         assertEquals(0, helper.filter(new HeartBeat[]{}).length);
+    }
+
+    @Test
+    public void FilterReturnsTheHeartBeatsFromTheCurrentRegion(){
+        val seededHeartBeats = HeartBeatFactory.Create(10);
+        seededHeartBeats[8].setRegion("us-test-2");
+        seededHeartBeats[9].setRegion("us-test-2");
+        val expected = Arrays.copyOfRange(seededHeartBeats, 0, 8);
+
+        val actual = helper.filter(seededHeartBeats);
+
+        ShouldMatch(expected, actual);
     }
 }
