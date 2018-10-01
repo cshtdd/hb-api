66afd67da1cd4ef3fe4bda85997bf916e8062b17
Camilo Sanchez <camilin87@gmail.com>
2018-10-01 08:10:38 -0400

    add a method to the NowReader for the EpochSecond

--- src/main/java/com/tddapps/utils/NowReader.java
+++ src/main/java/com/tddapps/utils/NowReader.java
@@ -4,4 +4,5 @@ import java.util.Date;
 
 public interface NowReader {
     Date ReadUtc();
+    long ReadEpochSecond();
 }
--- src/main/java/com/tddapps/utils/NowReaderImpl.java
+++ src/main/java/com/tddapps/utils/NowReaderImpl.java
@@ -2,11 +2,17 @@ package com.tddapps.utils;
 
 import java.util.Date;
 
+import static com.tddapps.utils.DateExtensions.EpochSecondsNow;
 import static com.tddapps.utils.DateExtensions.UtcNow;
 
 public class NowReaderImpl implements NowReader {
     @Override
     public Date ReadUtc() {
         return UtcNow();
+    }
+
+    @Override
+    public long ReadEpochSecond() {
+        return EpochSecondsNow();
     }
 }
