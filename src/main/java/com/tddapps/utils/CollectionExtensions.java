1d500b7e4c69374e9aa4f98b8a443bbdebcde3c3
Camilo Sanchez <camilin87@gmail.com>
2018-10-12 00:46:21 -0400

    the CollectionExtensions return Lists

--- src/main/java/com/tddapps/utils/CollectionExtensions.java
+++ src/main/java/com/tddapps/utils/CollectionExtensions.java
@@ -1,17 +1,18 @@
 package com.tddapps.utils;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public abstract class CollectionExtensions {
-    public static <T> Collection<T> Intersection(Collection<T> l1, Collection<T> l2){
+    public static <T> List<T> Intersection(Collection<T> l1, Collection<T> l2){
         return l1
                 .stream()
                 .filter(l2::contains)
                 .collect(Collectors.toList());
     }
 
-    public static <T> Collection<T> Difference(Collection<T> all, Collection<T> subset){
+    public static <T> List<T> Difference(Collection<T> all, Collection<T> subset){
         return all
                 .stream()
                 .filter(e -> !subset.contains(e))
