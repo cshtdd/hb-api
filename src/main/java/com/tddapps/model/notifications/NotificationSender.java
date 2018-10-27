17345630461c2a7a3b60d3a8fa67bc2325b1de34
Camilo Sanchez <camilin87@gmail.com>
2018-10-27 11:29:26 -0400

    optimize import

--- src/main/java/com/tddapps/model/notifications/NotificationSender.java
+++ src/main/java/com/tddapps/model/notifications/NotificationSender.java
@@ -1,7 +1,6 @@
 package com.tddapps.model.notifications;
 
 import com.tddapps.model.DalException;
-import com.tddapps.model.notifications.Notification;
 
 public interface NotificationSender {
     void Send(Notification notification) throws DalException;
