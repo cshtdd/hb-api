a1741b23f32d20e88a0190657cea9b4b47b8abf4
Camilo Sanchez <camilin87@gmail.com>
2020-02-14 09:05:12 -0500

    Create the first sample integration test that leverages localstack
    https://github.com/localstack/localstack

---
+++ src/test/java/com/tddapps/model/internal/aws/SQSIntegrationTest.java
@@ -0,0 +1,53 @@
+package com.tddapps.model.internal.aws;
+
+import cloud.localstack.docker.LocalstackDockerExtension;
+import cloud.localstack.docker.annotation.IEnvironmentVariableProvider;
+import cloud.localstack.docker.annotation.LocalstackDockerProperties;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.Message;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(LocalstackDockerExtension.class)
+@LocalstackDockerProperties(services = { "sqs" }, environmentVariableProvider = SQSIntegrationTest.TestEnvironment.class)
+public class SQSIntegrationTest {
+    public static class TestEnvironment implements IEnvironmentVariableProvider {
+        @Override
+        public Map<String, String> getEnvironmentVariables() {
+            return new HashMap<String, String>(){{
+                put("DEFAULT_REGION", "test_sandbox");
+            }};
+        }
+    }
+
+    @Test
+    void SampleTest1(){
+        val sqs = AmazonSQSClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "test_sandbox"))
+                .build();
+        sqs.createQueue(new CreateQueueRequest("sample_queue_1"));
+
+        val queueUrl = sqs.listQueues().getQueueUrls().get(0);
+
+        sqs.sendMessage(queueUrl, "Sample Message1");
+
+        val messages = sqs.receiveMessage(queueUrl).getMessages();
+
+        val receivedBody = messages.get(0).getBody();
+
+        messages.forEach(m -> sqs.deleteMessage(queueUrl, m.getReceiptHandle()));
+
+        sqs.deleteQueue(queueUrl);
+
+        assertEquals("Sample Message1", receivedBody);
+    }
+}
