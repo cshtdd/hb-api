9a1fc958e7cad62b8018cdc5f073cf701eb307dc
Camilo Sanchez <camilin87@gmail.com>
2020-02-18 07:19:30 -0500

    Create a new Setting constant for the DEFAULT_REGION

--- src/main/java/com/tddapps/model/infrastructure/Settings.java
+++ src/main/java/com/tddapps/model/infrastructure/Settings.java
@@ -2,6 +2,7 @@ package com.tddapps.model.infrastructure;
 
 public abstract class Settings {
     public final static String AWS_REGION = "AWS_REGION";
+    public final static String DEFAULT_REGION = "DEFAULT_REGION";
     public final static String TOPIC_NAME = "TOPIC_NAME";
     public final static String TABLE_PREFIX = "TABLE_PREFIX";
     public final static String DYNAMO_DB_ENDPOINT_OVERRIDE = "DYNAMO_DB_ENDPOINT_OVERRIDE";
